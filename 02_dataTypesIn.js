8d850c1c094487f59f1b8e3bf652d5607e19df5f
Shubham jha <sjha870054@gmail.com>
2023-09-20 15:33:56 +0530

    dataType

---
+++ 02_dataTypesIn.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert ( 3 + 3) // we are using nordjs, not browser
+
+console.log(3 + 3), console.log("Shubham");
+//  code readability should be high
+
+// dataType
+
+let name = "Shubham";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2 to power 53(range)
+// bigint => largest number
+// string =>""
+// bollean => true/false
+// null   => standalone value
+// undefined => value not assign
+// symbol => uniqe
+
+// object
+
+console.log(typeof null); /* Object */
+console.log(typeof undefined); /* underfine */
